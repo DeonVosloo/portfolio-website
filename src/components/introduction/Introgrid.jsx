ab025580de528a8cc13de3f0a6425e7d480e569b
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-14 13:30:45 +0200

    Fixed banner not displaying correctly on mobile

--- src/components/introduction/Introgrid.jsx
+++ src/components/introduction/Introgrid.jsx
@@ -10,7 +10,7 @@ export default function Introgrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} sx={{display: "flex", justifyContent: "center"}}>
-      <Grid item xs={12} sm={11.5}  md={1} lg={9.5}> 
+      <Grid item xs={12} sm={11.5}  md={10} lg={9.5}> 
           <Banner/>
         </Grid>
         <Grid item xs={12} sm={11.5} md={10.5} lg={8} sx={{marginTop: "6%"}} >
